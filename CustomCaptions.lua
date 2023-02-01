6bdc9cd7e336d72664fbf2ffa42c41194bf3b7d5
03cubeGD <84852206+03cubeGD@users.noreply.github.com>
2023-02-01 00:10:52 -0600

    Update CustomCaptions.lua

--- CustomCaptions.lua
+++ CustomCaptions.lua
@@ -1,7 +1,7 @@
 -- Instances:
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
---local TextCustom = "*jiggles your balls*"
+local TextCustom = "*jiggles your balls*"
 local CustomCaption = Instance.new("ScreenGui")
 local Caption = Instance.new("TextLabel")
 local UIPadding = Instance.new("UIPadding")
